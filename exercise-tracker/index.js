0bb4ef452710ce1b95d35d68cf40505d5734acfb
nideshpandey <nideshpy@gmail.com>
2022-12-30 17:49:04 +0545

    Returns exercise response

--- exercise-tracker/index.js
+++ exercise-tracker/index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const User = require("./models/User");
 const Exercise = require("./models/Exercise");
+const Log = require("./models/Log");
 
 const app = express();
 app.use(cors());
@@ -53,47 +54,107 @@ app.get("/api/users", async (req, res) => {
 
 // POST exercise information
 app.post("/api/users/:_id/exercises", async (req, res) => {
-  const id = req.params._id;
-  //console.log(id);
+  const _id = req.params._id;
+  //console.log(_id);
 
-  let user = await User.findOne({ id });
-  console.log(user);
-  res.json("Ok");
+  try {
+    let user = await User.findOne({ _id });
+    //console.log(user);
 
-  //   try {
-  //     let user = await User.findById({ _id: req.params._id });
-  //     //console.log(user);
+    if (user) {
+      let { description, duration, date } = req.body;
+      const { _id, username } = user;
+      //console.log(_id, username);
 
-  //     if (user) {
-  //       let { description, duration, date } = req.body;
-  //       const { _id, username } = user;
-  //       console.log(_id, username);
+      let new_date;
 
-  //       let new_date;
+      if (date) {
+        new_date = new Date(date).toDateString();
+      } else {
+        new_date = new Date().toDateString();
+      }
 
-  //       if (date) {
-  //         date = new Date(date).toDateString();
-  //       } else {
-  //         date = new Date().toDateString();
-  //       }
+      //console.log(new_date);
 
-  //       const exercise = new Exercise({
-  //         username,
-  //         description,
-  //         duration,
-  //         date,
-  //       });
+      const exercise = new Exercise({
+        username: username,
+        description,
+        duration,
+        date: new_date,
+        _id: _id,
+      });
 
-  //       await exercise.save();
-  //       res.json(exercise);
-  //     } else {
-  //       console.log("User not found. Create a user first");
-  //       res.json("User not found. Create a user first");
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.status(500).json("Server Error !");
-  //   }
+      // let logDetail = await Log.findOne({ _id });
+      // let logObj = {
+      //   description,
+      //   duration,
+      //   date: new_date,
+      // };
+
+      // if (logDetail) {
+      //   let countExercise = logDetail.count;
+      //   let { log } = logDetail;
+      //   const logs = new Log({
+      //     username: username,
+      //     count: countExercise++,
+      //     _id: _id,
+      //     log: log.append(logObj),
+      //   });
+      //   await logs.save();
+      // } else {
+      //   let { log } = logDetail;
+      //   const logs = new Log({
+      //     username: username,
+      //     count: 1,
+      //     _id: _id,
+      //     log: log.append(logObj),
+      //   });
+      //   await logs.save();
+      // }
+
+      await exercise.save();
+
+      res.json(exercise);
+    } else {
+      console.log("User not found. Create a user first");
+      res.json("User not found. Create a user first");
+    }
+  } catch (err) {
+    console.log(err.name);
+    if ((err.name = "CastError")) {
+      console.log("Id is invalid !");
+      res.json("Invalid Id !");
+    } else {
+      console.log(err);
+      res.status(500).json("Server Error !");
+    }
+  }
+});
+
+// GET request to retrieve full exercise log
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const _id = req.params._id;
+  console.log(_id);
+
+  try {
+    let user = await User.findOne({ _id });
+    //console.log(log);
+    if (user) {
+      console.log("User Log Found !");
+    } else {
+      console.log("User not found !");
+      res.json("User not Found");
+    }
+  } catch (err) {
+    console.log(err.name);
+    if ((err.name = "CastError")) {
+      console.log("Id is invalid !");
+      res.json("Invalid Id !");
+    } else {
+      console.log(err);
+      res.status(500).json("Server Error !");
+    }
+  }
 });
 
 app.listen(process.env.PORT, () => {
