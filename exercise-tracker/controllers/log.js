b9a5291649a0a87ad570aa74a29738641b2d69cd
nideshpandey <nideshpy@gmail.com>
2023-01-02 13:35:35 +0545

    Code refactoring

--- exercise-tracker/controllers/log.js
+++ exercise-tracker/controllers/log.js
@@ -31,50 +31,6 @@ const getLog = async (req, res) => {
       console.log("No exercises saved until now");
       res.json("No exercises saved until no");
     }
-
-    // if (temp) {
-    //   let log = await Log.findOne({ user: id });
-    //   console.log(log);
-
-    //   if (log) {
-    //     let tempArr = [];
-    //     temp.map((item) => {
-    //       tempArr.push({
-    //         description: item.description,
-    //         duration: item.duration,
-    //         date: item.date,
-    //       });
-    //       //console.log(item.description, item.duration, item.date);
-    //     });
-
-    //     let log = await Log.findOneAndUpdate({
-    //       count: tempArr.length,
-    //       log: tempArr,
-    //     });
-    //     res.json(log);
-    //   } else {
-    //     let tempArr = [];
-    //     temp.map((item) => {
-    //       tempArr.push({
-    //         description: item.description,
-    //         duration: item.duration,
-    //         date: item.date,
-    //       });
-    //       //console.log(item.description, item.duration, item.date);
-    //     });
-
-    //     const log = new Log({
-    //       user: id,
-    //       count: temp.length,
-    //       log: tempArr,
-    //     });
-    //     await log.save();
-    //     res.json(log);
-    //   }
-    // } else {
-    //   console.log("No exercises saved until now");
-    //   res.json("No exercises saved until no");
-    // }
   } catch (err) {
     console.log(err);
     res.status(500).json("Server Error !");
